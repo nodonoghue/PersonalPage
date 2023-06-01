a34cc2b8a3161351e40a2f34266ff6caf6b5d1eb
nodonoghue <nodonoghue@protonmail.com>
2023-05-31 22:45:58 -0700

    WIP: Working on reading local json file

--- src/app/services/data/json/json-data-reader.service.ts
+++ src/app/services/data/json/json-data-reader.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JsonDataReaderService {
 
-  constructor() { }
+  jsonData: any;
+
+  constructor(private http: HttpClient) {
+    this.jsonData = this.getJSON().subscribe(data => { console.log("Data", data) });
+  }
+
+  public getJSON(): Observable<any> {
+    return this.http.get("../../../data/json/sample.json");
+  }
 }
