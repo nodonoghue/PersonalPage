61107cd0c8f88510d263ac75b992f55a7946d214
nodonoghue <nodonoghue@protonmail.com>
2023-06-12 20:51:24 -0700

    Too sick to concentrate, not much here with this one.

--- src/app/components/musings/musing-template/musing-template.component.ts
+++ src/app/components/musings/musing-template/musing-template.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { JsonDataReaderService } from '../../../services/data/json/json-data-reader.service';
 @Component({
   selector: 'app-musing-template',
   templateUrl: './musing-template.component.html',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MusingTemplateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private jsonReader: JsonDataReaderService) {
+
+  }
+
+  musings: any;
 
   ngOnInit(): void {
-
+    this.musings = this.jsonReader.getMusingsJson();
   }
 }
